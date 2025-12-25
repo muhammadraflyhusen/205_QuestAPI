3101becebe7ab2dc7842e4e867423702365e896b
muhammadraflyhusen <raflyhusen311@gmail.com>
2025-12-25 21:30:12 +0700

    menambahkan destinasi navigasi entry siswa

---
+++ app/src/main/java/com/example/restapi2/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,6 @@
+package com.example.restapi2.uicontroller.route
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
